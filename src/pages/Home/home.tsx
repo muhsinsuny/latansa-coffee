34368aa02155184612bc8e9367a7a2c148a28ce5
muhsinsuny <muhsinsuny@gmail.com>
2025-04-08 14:02:03 +0700

    editing

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div className='h-screen bg-gray-100'>
+      <h1 className='text-4xl font-bold align-middle text-center text-gray-800'>
+        Welcome to Latansa Coffee
+      </h1>
+      home
+    </div>
+  );
+};
+
+export default Home;
